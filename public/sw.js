a46b44d99ec751db9994d5a5b92add6ce699dc8f
sachinsaitej7 <63497257+sachinsaitej7@users.noreply.github.com>
2022-12-18 05:58:18 +0530

    PWA update

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+// service worker to make a web app installable
+
+// Installing service worker
+const CACHE_NAME = "Seller App";
+
+/* Add relative URL of all the static content you want to store in
+ * cache storage (this will help us use our app offline)*/
+let resourcesToCache = ["./Favicon.png", "./logo-192.png", "./logo-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(resourcesToCache);
+    })
+  );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+// Update a service worker
+const cacheWhitelist = ["Seller App"];
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
